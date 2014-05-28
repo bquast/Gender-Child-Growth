b6cb955f096f88f782141cc917803dae51658ce0
Bastiaan Quast <bquast@gmail.com>
2014-05-28 10:18:23 +0200

    first import code

---
+++ import.R
@@ -0,0 +1,8 @@
+# import.R
+# --------------------------------------------------------
+# instructions for importing the SPSS files
+# Bastiaan Quast
+# bquast@gmail.com
+
+# load the foreign package
+library(foreign)
