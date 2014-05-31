48d5fd6ac1a2e1512c70cf8e949c25902402512d
Bastiaan Quast <bquast@gmail.com>
2014-05-31 13:56:16 +0200

    fix Ind der, woman -> best.woman

--- transform.R
+++ transform.R
@@ -35,31 +35,33 @@ table(Child.W1$w1.c.weight.3)
 length(table(Child.W1$w1.c.weight.3))
 str(Child.W1$w1.c.weight.3)
 
+
+### possibly treat these same as the other recoded vars ####
 # encode month factor variables as numeric
 Child.W1$w1.c.dob.m <- as.numeric(Child.W1$w1.c.dob.m)
 Child.W1$w1.c.intrv.m <- as.numeric(Child.W1$w1.c.intrv.m)
 
 # filter out the missing value codes and write them to separate variables
 attach(Child.W1)
-Child.W1$w1.c.dob.y.c <- ifelse(w1.c.dob.y >= 2020, w1.c.dob.y, NA)
+Child.W1$w1.c.dob.y.flg <- ifelse(w1.c.dob.y >= 2020, w1.c.dob.y, NA)
 Child.W1$w1.c.dob.y <- ifelse(w1.c.dob.y <= 2020, w1.c.dob.y, NA)
-Child.W1$w1.c.dob.m.c <- ifelse(Child.W1$w1.c.dob.m > 12, w1.c.dob.m, NA)
+Child.W1$w1.c.dob.m.flg <- ifelse(Child.W1$w1.c.dob.m > 12, w1.c.dob.m, NA)
 Child.W1$w1.c.dob.m <- ifelse(w1.c.dob.m <= 12, w1.c.dob.m, NA)
-Child.W1$w1.c.intrv.y.c <- ifelse(w1.c.intrv.y <= 0, w1.c.intrv.y, NA)
+Child.W1$w1.c.intrv.y.flg <- ifelse(w1.c.intrv.y <= 0, w1.c.intrv.y, NA)
 Child.W1$w1.c.intrv.y <- ifelse(w1.c.intrv.y >= 0, w1.c.intrv.y, NA)
-Child.W1$w1.c.intrv.m.c <- ifelse(w1.c.intrv.m > 12, w1.c.intrv.m, NA)
+Child.W1$w1.c.intrv.m.flg <- ifelse(w1.c.intrv.m > 12, w1.c.intrv.m, NA)
 Child.W1$w1.c.intrv.m <- ifelse(w1.c.intrv.m <= 12, w1.c.intrv.m, NA)
-Child.W1$w1.c.intrv.d.c <- ifelse(w1.c.intrv.d > 31, w1.c.intrv.d, NA)
+Child.W1$w1.c.intrv.d.flg <- ifelse(w1.c.intrv.d > 31, w1.c.intrv.d, NA)
 Child.W1$w1.c.intrv.d <- ifelse(w1.c.intrv.d <= 31, w1.c.intrv.d, NA)
-Child.W1$w1.c.height.1.c <- ifelse(w1.c.height.1 <= 0, w1.c.height.1, NA)
-Child.W1$w1.c.height.2.c <- ifelse(w1.c.height.2 <= 0, w1.c.height.2, NA)
-Child.W1$w1.c.height.3.c <- ifelse(w1.c.height.3 <= 0, w1.c.height.3, NA)
+Child.W1$w1.c.height.1.flg <- ifelse(w1.c.height.1 <= 0, w1.c.height.1, NA)
+Child.W1$w1.c.height.2.flg <- ifelse(w1.c.height.2 <= 0, w1.c.height.2, NA)
+Child.W1$w1.c.height.3.flg <- ifelse(w1.c.height.3 <= 0, w1.c.height.3, NA)
 Child.W1$w1.c.height.1 <- ifelse(w1.c.height.1 >= 0, w1.c.height.1, NA)
 Child.W1$w1.c.height.2 <- ifelse(w1.c.height.2 >= 0, w1.c.height.2, NA)
 Child.W1$w1.c.height.3 <- ifelse(w1.c.height.3 >= 0, w1.c.height.3, NA)
-Child.W1$w1.c.weight.1.c <- ifelse(w1.c.weight.1 <= 0, w1.c.weight.1, NA)
-Child.W1$w1.c.weight.2.c <- ifelse(w1.c.weight.2 <= 0, w1.c.weight.2, NA)
-Child.W1$w1.c.weight.3.c <- ifelse(w1.c.weight.3 <= 0, w1.c.weight.3, NA)
+Child.W1$w1.c.weight.1.flg <- ifelse(w1.c.weight.1 <= 0, w1.c.weight.1, NA)
+Child.W1$w1.c.weight.2.flg <- ifelse(w1.c.weight.2 <= 0, w1.c.weight.2, NA)
+Child.W1$w1.c.weight.3.flg <- ifelse(w1.c.weight.3 <= 0, w1.c.weight.3, NA)
 Child.W1$w1.c.weight.1 <- ifelse(w1.c.weight.1 >= 0, w1.c.weight.1, NA)
 Child.W1$w1.c.weight.2 <- ifelse(w1.c.weight.2 >= 0, w1.c.weight.2, NA)
 Child.W1$w1.c.weight.3 <- ifelse(w1.c.weight.3 >= 0, w1.c.weight.3, NA)
@@ -90,7 +92,7 @@ str(Individual.Derived.W1$w1.best.gen)
 Individual.Derived.W1$w1.best.gen <- ifelse(as.numeric(Individual.Derived.W1$w1.best.gen) %in% 5:6, Individual.Derived.W1$w1.best.gen, NA)
 str(Individual.Derived.W1$w1.best.gen)
 summary(Individual.Derived.W1$w1.best.gen)
-Individual.Derived.W1$w1.best.woman <- Individual.Derived.W1$w1.best.gen == 'Female'
+Individual.Derived.W1$w1.best.woman <- Individual.Derived.W1$w1.best.gen == 6
 summary(Child.W1$w1.c.woman)
 summary(Adult.W1$w1.a.woman)
 summary(Household.Roster.W1$w1.r.woman)
@@ -101,21 +103,21 @@ summary(Individual.Derived.W1$w1.best.woman)
 Individual.Derived.W1$w1.spen.d <- ifelse(is.na(Individual.Derived.W1$w1.spen), FALSE, TRUE)
 # construct woman and man pension dummies
 # list the household IDs with a female pension recipient
-w1.spen.d.w.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.spen.d == TRUE & Individual.Derived.W1$w1.woman == TRUE),]$w1.hhid
+w1.spen.d.w.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.spen.d == TRUE & Individual.Derived.W1$w1.best.woman == TRUE),]$w1.hhid
 # list the household IDs with a male pension recipient
-w1.spen.d.m.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.spen.d == TRUE & Individual.Derived.W1$w1.woman == FALSE),]$w1.hhid
+w1.spen.d.m.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.spen.d == TRUE & Individual.Derived.W1$w1.best.woman == FALSE),]$w1.hhid
 # list the household IDs with awoman aged 60+
-w1.woman.60.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.woman == TRUE & Individual.Derived.W1$w1.best.age.yrs >= 60),]$w1.hhid
+w1.woman.60.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.best.woman == TRUE & Individual.Derived.W1$w1.best.age.yrs >= 60),]$w1.hhid
 # list the household IDs with a man agen 60+
-w1.man.60.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.woman == FALSE & Individual.Derived.W1$w1.best.age.yrs >= 60),]$w1.hhid
+w1.man.60.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.best.woman == FALSE & Individual.Derived.W1$w1.best.age.yrs >= 60),]$w1.hhid
 # list the household IDs with a woman agen 65+
-w1.woman.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.woman == TRUE & Individual.Derived.W1$w1.best.age.yrs >= 65),]$w1.hhid
+w1.woman.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.best.woman == TRUE & Individual.Derived.W1$w1.best.age.yrs >= 65),]$w1.hhid
 # list the household IDs with a man agen 65+
-w1.man.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.woman == FALSE & Individual.Derived.W1$w1.best.age.yrs >= 65),]$w1.hhid
+w1.man.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.best.woman == FALSE & Individual.Derived.W1$w1.best.age.yrs >= 65),]$w1.hhid
 # list the household IDs with a woman agen 60-64
-w1.woman.60.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.woman == TRUE & Individual.Derived.W1$w1.best.age.yrs >= 60 & Individual.Derived.W1$w1.best.age.yrs < 65),]$w1.hhid
+w1.woman.60.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.best.woman == TRUE & Individual.Derived.W1$w1.best.age.yrs >= 60 & Individual.Derived.W1$w1.best.age.yrs < 65),]$w1.hhid
 # list the household IDs with a man agen 60-64
-w1.man.60.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.woman == FALSE & Individual.Derived.W1$w1.best.age.yrs >= 60 & Individual.Derived.W1$w1.best.age.yrs < 65),]$w1.hhid
+w1.man.60.65.hhid <- Individual.Derived.W1[which(Individual.Derived.W1$w1.best.woman == FALSE & Individual.Derived.W1$w1.best.age.yrs >= 60 & Individual.Derived.W1$w1.best.age.yrs < 65),]$w1.hhid
 # this could also be done from the adult data set
 
 # create pension dummies in Child data frame
@@ -150,8 +152,19 @@ Child.W1$w1.id.spen <- Spen.IndD.W1$hh.spen[match (Child.W1$w1.hhid, Spen.IndD.W
 Child.W1$w1.id.spen.w <- Spen.IndD.W1[Spen.IndD.W1$w1.best.woman == TRUE,]$hh.spen[match (Child.W1$w1.hhid, Spen.IndD.W1$w1.hhid) ]
 Child.W1$w1.id.spen.m <- Spen.IndD.W1[Spen.IndD.W1$w1.best.woman == FALSE,]$hh.spen[match (Child.W1$w1.hhid, Spen.IndD.W1$w1.hhid) ]
 
+# merge the z-scores into the Child data frame
+Child.W1$w1.zhfa <- Individual.Derived.W1$w1.zhfa[match(Child.W1$pid, Individual.Derived.W1$pid)]
+Child.W1$w1.zwfh <- Individual.Derived.W1$w1.zwfh[match(Child.W1$pid, Individual.Derived.W1$pid)]
+Child.W1$w1.zwfa <- Individual.Derived.W1$w1.zwfa[match(Child.W1$pid, Individual.Derived.W1$pid)]
+Child.W1$w1.zbmi <- Individual.Derived.W1$w1.zbmi[match(Child.W1$pid, Individual.Derived.W1$pid)]
+Child.W1$w1.fwag <- Individual.Derived.W1$w1.fwag[match(Child.W1$pid, Individual.Derived.W1$pid)]
 
-#### MERGE household income, parent education, etc. ####
+# merge household income, parent education
+Child.W1$w1.fwag <- Individual.Derived.W1$w1.fwag[match(Child.W1$pid, Individual.Derived.W1$pid)]
+Child.W1$w1.h.tinc <- Household.Questionnaire.W1$w1.h.tinc[match(Child.W1$w1.hhid, Household.Questionnaire.W1$w1.hhid)]
+Child.W1$w1.best.edu <- Individual.Derived.W1$w1.best.edu[match(Child.W1$w1.c.respid, Individual.Derived.W1$pid)]
+Child.W1$w1.best.age.yrs <- Individual.Derived.W1$w1.best.age.yrs[match(Child.W1$w1.c.respid, Individual.Derived.W1$pid)]
+#### see what other covariates can be used ####
 
 
 ######
