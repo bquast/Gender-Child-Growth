7ef31d385c4363fbbf000cf9b4c9b0bc94f2a438
Bastiaan Quast <bquast@gmail.com>
2014-07-01 14:59:46 +0200

    add flags for 'dont know gender' etc.

--- transform.W2.R
+++ transform.W2.R
@@ -4,34 +4,13 @@
 # instructions for transforming the imported object to a tidy data frame
 
 # install and load the required packages
-install.packages('plyr')
-library(plyr)
-
+library(dplyr)
 
 # construct a length-of-month object
 month <- (3*365 + 366) / (4*12)
 
 # load the imported RData file
 load("imported.RData")
-
-#### EVALUATE these variables before reencoding ####
-summary(Child.W2$w2.c.height.1)
-
-summary(Child.W2$w2.c.height.2)
-
-summary(Child.W2$w2.c.height.3)
-table(Child.W2$w2.c.height.3)
-length(table(Child.W2$w2.c.height.3))
-str(Child.W2$w2.c.height.3)
-
-summary(Child.W2$w2.c.weight.1)
-
-summary(Child.W2$w2.c.weight.2)
-
-summary(Child.W2$w2.c.weight.3)
-table(Child.W2$w2.c.weight.3)
-length(table(Child.W2$w2.c.weight.3))
-str(Child.W2$w2.c.weight.3)
 
 
 ### possibly treat these same as the other recoded vars ####
@@ -67,23 +46,23 @@ detach(Child.W2)
 
 # filter out the erronous dob months
 Child.W2$w2.c.dob.m[199] <- NA
+#### report to NIDS ####
+
 
 # construct age in days and months
 Child.W2$w2.c.intrv.dt <- as.Date(paste(Child.W2$w2.c.intrv.y, Child.W2$w2.c.intrv.m, Child.W2$w2.c.intrv.d, sep="-"))
 Child.W2$w2.c.dob.dt <- as.Date(paste(Child.W2$w2.c.dob.y, Child.W2$w2.c.dob.m,1, sep="-"))
 Child.W2$w2.c.age.d <- as.numeric(Child.W2$w2.c.intrv.dt - Child.W2$w2.c.dob.dt)
 Child.W2$w2.c.age.m <- Child.W2$w2.c.age.d %/% month
-#### analyse these created variables and describe them above ####
-summary(Child.W2$w2.c.intrv.dt)
-summary(Child.W2$w2.c.dob.dt)
-summary(Child.W2$w2.c.age.d)
-summary(Child.W2$w2.c.age.m)
 
 # filter out missing value codes
-### move these to flags ####
+Child.W2$w2.c.gen.flg <- ifelse(!as.numeric(Child.W2$w2.c.gen) %in% 5:6, Child.W2$w2.c.gen, NA)
 Child.W2$w2.c.gen <- ifelse(as.numeric(Child.W2$w2.c.gen) %in% 5:6, Child.W2$w2.c.gen, NA)
+Adult.W2$w2.a.gen.flg <- ifelse(!as.numeric(Adult.W2$w2.a.gen) %in% 5:6, Adult.W2$w2.a.gen, NA)
 Adult.W2$w2.a.gen <- ifelse(as.numeric(Adult.W2$w2.a.gen) %in% 5:6, Adult.W2$w2.a.gen, NA)
+Household.Roster.W2$w2.r.gen.flg <- ifelse(!as.numeric(Household.Roster.W2$w2.r.gen) %in% 5:6, Household.Roster.W2$w2.r.gen, NA)
 Household.Roster.W2$w2.r.gen <- ifelse(as.numeric(Household.Roster.W2$w2.r.gen) %in% 5:6, Household.Roster.W2$w2.r.gen, NA)
+Individual.Derived.W2$w2.best.gen.flg <- ifelse(!as.numeric(Individual.Derived.W2$w2.best.gen) %in% 5:6, Individual.Derived.W2$w2.best.gen, NA)
 Individual.Derived.W2$w2.best.gen <- ifelse(as.numeric(Individual.Derived.W2$w2.best.gen) %in% 5:6, Individual.Derived.W2$w2.best.gen, NA)
 
 # create logical dummies
@@ -92,33 +71,18 @@ Adult.W2$w2.a.woman <- Adult.W2$w2.a.gen == 6
 Household.Roster.W2$w2.r.woman <- Household.Roster.W2$w2.r.gen == 6
 Individual.Derived.W2$w2.best.woman <- Individual.Derived.W2$w2.best.gen == 6
 
-# analyse the results
-summary(Child.W2$w2.c.woman)
-summary(Adult.W2$w2.a.woman)
-summary(Household.Roster.W2$w2.r.woman)
-summary(Individual.Derived.W2$w2.best.woman)
-
-
 # transform the pension variable into a dummy
 Individual.Derived.W2$w2.spen.d <- ifelse(is.na(Individual.Derived.W2$w2.spen), FALSE, TRUE)
+
 # construct woman and man pension dummies
-# list the household IDs with a female pension recipient
 w2.spen.d.w.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.spen.d == TRUE & Individual.Derived.W2$w2.best.woman == TRUE),]$w2.hhid
-# list the household IDs with a male pension recipient
 w2.spen.d.m.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.spen.d == TRUE & Individual.Derived.W2$w2.best.woman == FALSE),]$w2.hhid
-# list the household IDs with awoman aged 60+
 w2.woman.60.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.best.woman == TRUE & Individual.Derived.W2$w2.best.age.yrs >= 60),]$w2.hhid
-# list the household IDs with a man agen 60+
 w2.man.60.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.best.woman == FALSE & Individual.Derived.W2$w2.best.age.yrs >= 60),]$w2.hhid
-# list the household IDs with a woman agen 65+
 w2.woman.65.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.best.woman == TRUE & Individual.Derived.W2$w2.best.age.yrs >= 65),]$w2.hhid
-# list the household IDs with a man agen 65+
 w2.man.65.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.best.woman == FALSE & Individual.Derived.W2$w2.best.age.yrs >= 65),]$w2.hhid
-# list the household IDs with a woman agen 60-64
 w2.woman.60.65.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.best.woman == TRUE & Individual.Derived.W2$w2.best.age.yrs >= 60 & Individual.Derived.W2$w2.best.age.yrs < 65),]$w2.hhid
-# list the household IDs with a man agen 60-64
 w2.man.60.65.hhid <- Individual.Derived.W2[which(Individual.Derived.W2$w2.best.woman == FALSE & Individual.Derived.W2$w2.best.age.yrs >= 60 & Individual.Derived.W2$w2.best.age.yrs < 65),]$w2.hhid
-# this could also be done from the adult data set
 
 # create pension dummies in Child data frame
 Child.W2$w2.spen.d.w <- Child.W2$w2.hhid %in% w2.spen.d.w.hhid
@@ -129,9 +93,6 @@ Child.W2$w2.woman.65 <- Child.W2$w2.hhid %in% w2.woman.65.hhid
 Child.W2$w2.man.65 <- Child.W2$w2.hhid %in% w2.man.65.hhid
 Child.W2$w2.woman.60.65 <- Child.W2$w2.hhid %in% w2.woman.60.65.hhid
 Child.W2$w2.man.60.65 <- Child.W2$w2.hhid %in% w2.man.60.65.hhid
-#### ANALYSE these variables, add descriptions above each line ####
-
-
 
 # filter out missing value codes for the adults raw data set
 summary(Adult.W2$w2.a.incgovpen.v)
@@ -139,8 +100,6 @@ attach(Adult.W2)
 Adult.W2$w2.a.incgovpen.v.flg <- ifelse(w2.a.incgovpen.v < 0, w2.a.incgovpen.v, NA)
 Adult.W2$w2.a.incgovpen.v <- ifelse(w2.a.incgovpen.v >= 0, w2.a.incgovpen.v, NA)
 detach(Adult.W2)
-summary(Adult.W2$w2.a.incgovpen.v)
-summary(Adult.W2$w2.a.incgovpen.v.flg)
 
 # create dataframe of household pension income (by gender)
 Spen.A.W2 <- ddply(Adult.W2, .(w2.hhid, w2.a.woman), summarize, hh.spen = sum(w2.a.incgovpen.v))
