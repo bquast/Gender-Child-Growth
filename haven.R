c4a7d48797cc2fff3bdd78f1e72b9cc9694e69ec
Bastiaan Quast <bquast@gmail.com>
2015-03-10 10:59:34 +0100

    add SAS read

--- haven.R
+++ haven.R
@@ -12,3 +12,5 @@ Child_W1_Stata <- read_sav("stata12/Child_W1_Anon_V5.2.dta")
 # test read SPSS
 Child_W1_SPSS <- read_spss("spss/Child_W1_Anon_V5.2.sav")
 
+# test read SAS
+Child_W1_SAS <- read_sas("sas/Child_W1_Anon_V5_2.sas7bdat")
