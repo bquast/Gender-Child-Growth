9417dc232c2cdd9c32836cdf9ada54114b89026e
Bastiaan Quast <bquast@gmail.com>
2015-11-16 09:00:43 +0100

    add data loading

--- R/train.R
+++ R/train.R
@@ -5,11 +5,13 @@
 # Bastiaan Quast
 # bquast@gmail.com
 
+# load the data
+load(file = 'data/child_sas.RData')
+
 # model specification
 m1 <- formula(zhfa ~ c_gen)
 
-
-# basic estimation
+# basic linear model estimation
 pls1 <- lm(formula = m1, data = childder1)
 
 # neural networks
@@ -18,21 +20,21 @@ library(neuralnet)
 library(NeuralNetTools)
 library(caret)
 
-# using nnet
+## using nnet
 nn1  <- nnet(formula = m1, data = childder1, size = 1)
 plotnet(nn1)
 
-# using neuralnet
+## using neuralnet
 nn1a <- neuralnet(formula = m1, data = childder1, hidden = 2)
 plotnet(nn1a)
 plot(nn1a, rep = 'best')
 
-# using caret to use nnet
+## using caret to use nnet
 c1 <- train(form = m1, data = childder1, method = 'nnet')
 plotnet(c1)
 plot(c1)
 
-# using caret to use neuralnet
+## using caret to use neuralnet
 c1a <- train(form = m1, data = childder1, method = 'neuralnet')
 plotnet(c1a)
 plot(c1a)
