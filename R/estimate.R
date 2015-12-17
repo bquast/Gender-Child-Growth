bdbf06fa25ac6c9d1d00532b82d7c96513508ed1
Bastiaan Quast <bquast@gmail.com>
2015-12-17 15:25:55 +0100

    setup new estimation file

---
+++ R/estimate.R
@@ -0,0 +1,17 @@
+# estimate.R
+# ------------------------------
+# instructions for estimation
+#
+# Bastiaan Quast
+# bquast@gmail.com
+
+
+# load libraries
+library(magrittr)
+library(dplyr)
+library(plm)
+
+
+# load data
+load('data/merged.RData')
+
