07744dd0a8a9ef08a056c87b730fe030ab746313
Bastiaan Quast <bquast@gmail.com>
2015-12-17 16:15:23 +0100

    spacing

--- R/estimate.R
+++ R/estimate.R
@@ -16,7 +16,7 @@ library(plm)
 load('data/merged.RData')
 
 
-# 
+# height for age
 zhfa1 <- plm(zhfa ~ spen_man*post_treatment + spen_woman + hhincome, child, model="within")
 zhfa2 <- plm(zhfa ~ spen_man*post_treatment + spen_woman + hhincome, child, model="between")
 zhfa3 <- plm(zhfa ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="between")
@@ -24,6 +24,8 @@ summary(zhfa1)
 summary(zhfa2)
 summary(zhfa3)
 
+
+# weight for height
 zwfa1 <- plm(zwfa ~ spen_man*post_treatment + spen_woman + hhincome, child, model="within")
 zwfa2 <- plm(zwfa ~ spen_man*post_treatment + spen_woman + hhincome, child, model="between")
 zwfa3 <- plm(zwfa ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="within")
@@ -33,6 +35,8 @@ summary(zwfa2)
 summary(zwfa3)
 summary(zwfa4)
 
+
+# weight for height
 zwfh1 <- plm(zwfh ~ spen_man*post_treatment + spen_woman + hhincome, child, model="within")
 zwfh2 <- plm(zwfh ~ spen_man*post_treatment + spen_woman + hhincome, child, model="between")
 zwfh3 <- plm(zwfh ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="within")
@@ -41,3 +45,14 @@ summary(zwfh1)
 summary(zwfh2)
 summary(zwfh3)
 summary(zwfh4)
+
+
+# BMI (Body Mass Index)
+zbmi1 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, model="within")
+zbmi2 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, model="between")
+zbmi3 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="within")
+zbmi4 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="between")
+summary(zbmi1)
+summary(zbmi2)
+summary(zbmi3)
+summary(zbmi4)
