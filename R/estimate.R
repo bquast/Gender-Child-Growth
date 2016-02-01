fd32aca4e61646f5e48bdec264c7e4138eb6096a
Bastiaan Quast <bquast@gmail.com>
2016-02-01 12:13:06 +0100

    further explore broken down by 60-65 and 65+

--- R/estimate.R
+++ R/estimate.R
@@ -40,14 +40,14 @@ zwfa3 <- plm(zwfa ~ spen_man*post_treatment + spen_woman + hhincome, child, best
 zwfa4 <- plm(zwfa ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="between")
 zwfa5 <- plm(zwfa ~ spen_man + spen_woman*post_treatment + hhincome, child, best_age_yrs < 6, model="between")
 zwfa6 <- plm(zwfa ~ spen_man*post_treatment + spen_woman*post_treatment + hhincome, child, best_age_yrs < 6, model="between")
-# zwfa7 <- nnet(zwfa ~ spen_man + spen_woman + hhincome + post_treatment, child[which(child$best_age_yrs < 6),], size = 3)
+zwfa7 <- plm(zwfa ~ spen_man_60_65 + spen_man_65 + spen_woman_60_65 + spen_woman_65 + hhincome, child, best_age_yrs < 6, model="between")
 summary(zwfa1)
 summary(zwfa2)
 summary(zwfa3)
 summary(zwfa4) # use this one
 summary(zwfa5) # use this one, does not work for women
 summary(zwfa6) # use this one
-# plotnet(zwfa7)
+summary(zwfa7)
 
 ## weight for height
 zwfh1 <- plm(zwfh ~ spen_man*post_treatment + spen_woman + hhincome, child, model="within")
@@ -56,20 +56,24 @@ zwfh3 <- plm(zwfh ~ spen_man*post_treatment + spen_woman + hhincome, child, best
 zwfh4 <- plm(zwfh ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="between")
 zwfh5 <- plm(zwfh ~ spen_man + spen_woman*post_treatment + hhincome, child, best_age_yrs < 6, model="between")
 zwfh6 <- plm(zwfh ~ spen_man*post_treatment + spen_woman*post_treatment + hhincome, child, best_age_yrs < 6, model="between")
+zwfh7 <- plm(zwfh ~ spen_man_60_65 + spen_man_65 + spen_woman_60_65 + spen_woman_65 + hhincome, child, best_age_yrs < 6, model="between")
 summary(zwfh1) # these all do no work, suggesting the effect is more long run
 summary(zwfh2)
 summary(zwfh3)
 summary(zwfh4)
 summary(zwfh5)
 summary(zwfh6)
+summary(zwfh7) # something positive for woman_65, same as BMI
 
 ## BMI (Body Mass Index)
 zbmi1 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, model="within")
 zbmi2 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, model="between")
 zbmi3 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="within")
 zbmi4 <- plm(zbmi ~ spen_man*post_treatment + spen_woman + hhincome, child, best_age_yrs < 6, model="between")
+zbmi5 <- plm(zbmi ~ spen_man_60_65 + spen_man_65 + spen_woman_60_65 + spen_woman_65 + hhincome, child, best_age_yrs < 6, model="between")
 summary(zbmi1) # these all do not work either also suggesting effect is long run, not short run
 summary(zbmi2)
 summary(zbmi3)
 summary(zbmi4)
+summary(zbmi5) # also works with within
 
